7db14e029a56ceece01022d04645d7a3fc165fbb
deilantrivino <deilan.trivino@gmail.com>
2014-10-23 21:29:52 -0300

    Deilan Historial - Primer Commit

--- app/src/main/java/machosoftware/proyectopos/MenuPrincipal.java
+++ app/src/main/java/machosoftware/proyectopos/MenuPrincipal.java
@@ -8,17 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-<<<<<<< HEAD:app/src/main/java/machosoftware/proyectopos/Main.java
-//DELETE
-
-public class Main extends Activity {
-=======
-/**
- * Menu principal.
- * Created by Sebastian on 27-09-2014.
- */
 public class MenuPrincipal extends Activity {
->>>>>>> 1354a100c283b0af2849336561e6fe54548443aa:app/src/main/java/machosoftware/proyectopos/MenuPrincipal.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
